fc58877a05b780a1c4185f4ed20f1950d1469fa5
Tushar Shekokar <tushar_s@tekditechnologies.com>
2020-03-27 10:40:20 +0530

    Feature #155571 feat: Sendy Techjoomla Integration Plugin - Resolve comments

--- tjsendy/tjsendy.php
+++ tjsendy/tjsendy.php
@@ -10,6 +10,7 @@ defined('_JEXEC') or die();
 
 use Joomla\CMS\Http\HttpFactory;
 use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
 jimport('joomla.event.plugin');
 
@@ -18,11 +19,25 @@ jimport('joomla.event.plugin');
  *
  * @since  __DEPLOY_VERSION__
  */
-class plgRDMediaTjsendy extends JPlugin
+class PlgRDMediaTjsendy extends JPlugin
 {
-
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 * @since  __DEPLOY_VERSION__
+	 */
 	protected $autoloadLanguage = true;
 
+	/**
+	 * Runs after process subscription
+	 *
+	 * @param   array  $data  An array containing the data of order.
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
 	public function onAfterProcessSubscription($data)
 	{
 		$productCat = $this->getCategory($data['product_id']);
@@ -38,34 +53,62 @@ class plgRDMediaTjsendy extends JPlugin
 			$array[$list->category] = $list->listid;
 		}
 
-		$this->subscribeUser($user,$array[$productCat]);
+		$this->subscribeUser($user, $array[$productCat]);
 	}
 
-	function getCategory($productId)
+	/**
+	 * function to get product category id
+	 *
+	 * @param   integer  $productId  product id.
+	 *
+	 * @return  integer  category id
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function getCategory($productId)
 	{
 		$db = Factory::getDbo();
 
 		$query = $db->getQuery(true)
 			->select('category_id')
 			->from($db->quoteName('#__rd_subs_product2category'))
-			->where($db->quoteName('product_id') . ' = ' . $db->quote($productId));
+			->where($db->quoteName('product_id') . ' = ' . (int) $productId);
 
 		$db->setQuery($query);
 
 		return $db->loadResult();
 	}
 
-
+	/**
+	 * function to add subscribe user in sendy
+	 *
+	 * @param   object   $user    user data.
+	 *
+	 * @param   integer  $listId  sendy list id.
+	 *
+	 * @return  string
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
 	public function subscribeUser($user, $listId)
-    {
-		$http = HttpFactory::getHttp();
-		$data = array();
-		$data['api_key'] = $this->params['api_key'];
-		$data['name'] = $user->name;
-		$data['email'] = $user->email;
-		$data['list'] = $listId;
-		$data['boolean'] = 'true';
+	{
+		try
+		{
+			$http = HttpFactory::getHttp();
+			$data = array();
+			$data['api_key'] = $this->params['api_key'];
+			$data['name'] = $user->name;
+			$data['email'] = $user->email;
+			$data['list'] = $listId;
+			$data['boolean'] = 'true';
 
-		$return = $http->post($this->params['sendy_url'] .'/subscribe', $data);
-    }
+			$return = $http->post($this->params['sendy_url'] . '/subscribe', $data);
+		}
+		catch (Exception $e)
+		{
+			JError::raiseWarning(500, Text::sprintf('PLG_TJSENDY_ERROR', $e->getMessage()));
+
+			return;
+		}
+	}
 }
